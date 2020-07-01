31a7437b1f8c3281c65aad3e7190fee1d37b7e0e
Damien Richard <DamienFr@users.noreply.github.com>
2020-07-01 17:02:29 +0400

    Update Main_homoplasy.sh

--- Main_homoplasy.sh
+++ Main_homoplasy.sh
@@ -1,5 +1,6 @@
 
-folder=dataset_5.1 #last dataset with NextStrain masking
+
+folder=dataset_17.1 # 24k iqtree NextStrain masking
 
 high_qual_homoplasies=./${folder}/homoplasies_to_keep.txt
 total_consistencyIndexReport=(./${folder}/consistencyIndexReport_*.txt)
@@ -13,9 +14,10 @@ echo $annotated_tree
 echo $alignement_file
 echo $reference_fasta_genome
 
-################################################################################################################
-############## This script produce a tree file keeping only selected homoplasies as annotations ################
-################################################################################################################
+############################################################################################################
+###############  SCRIPT 1 To use only if you want to use a subset of the homoplasies only ##################
+###### This script produce a tree file with spurious homoplasies removed from the annotations ##############
+############################################################################################################
 env high_qual_homoplasies=$high_qual_homoplasies total_consistencyIndexReport=$total_consistencyIndexReport perl -F',' -ane 'BEGIN{
 open (GA, "<", $ENV{high_qual_homoplasies}) or die "Cannot open input file $!";
 while ($line = <GA>) {
@@ -41,6 +43,23 @@ close FIL;
 foreach $key (keys %h_to_delete){
 $_ =~ s/(\)|-)$key(:|-)/$1$2/g; s/--/-/g ;  s/\)-:/\):/g ; s/\)-/\)/g ;
 } print ' $annotated_tree > ${annotated_tree}_filtered
+
+############################################################################################################
+###########  If you don't want to filter out any homoplasies, just do these two cp #########################
+############################################################################################################
+cp ${total_consistencyIndexReport} ${total_consistencyIndexReport}_filtered
+cp ${annotated_tree} ${annotated_tree}_filtered
+
+############################################################################################################
+###########  SCRIPT 2 produces a matrix with individuals in columns, homoplasic sites in lines #############
+########### and the matrix is filled with the allele types : "ref" not_ref or undef ########################
+############################################################################################################
+
+# output example :
+	EPI_ISL_43074	EPI_ISL_43074	EPI_ISL_43074	EPI_ISL_43074
+1912	"ref"	"ref"	"not_ref"	"ref"
+11083	"ref"	"not_ref"	"ref"	"undef"
+23043	"ref"	"ref"	"not_ref"	"ref"
 
 ###############################################################################################################
 ########### This script produces a matrix giving the allele of each homoplasy site for each isolate. ##########
@@ -87,6 +106,7 @@ if(lc($temp) eq "n"){ print "\tundef" ; $nnn++ }else{
 if(lc($temp) ne lc($allele_ref)){ print "\tnot_ref"}else{ print "\tref" }}
 } 
 print "\n";
+# print $fields[0] . "\t" . $allele_ref . "\n";
 }
 
 }; print STDERR "number of cumulative n at homo positions : $nnn\n";
@@ -101,14 +121,10 @@ close GB; ' > ./${folder}/input_matrix
 ########## having "ref", "not_ref" or "undef" alleles based on the input matrix. #####################
 ########## To be considered, an internal node must meet the following criterion : ####################
 ############# * No children nodes themselves annotated as carrying the homoplasy. ####################
-############# * Having at least three descendant tips. ###############################################
+############# * Having at least two descendant tips of each allele ###################################
 ############# * An homoplasic position is only considered for RoHo score computation if ##############
-################ at least n=5 or n=10 nodes satisfy the two first criteria. ##########################
+################ at least n=3 or n=5 nodes satisfy the two first criteria. ###########################
 ######################################################################################################
 
-
 Rscript --vanilla Homoplasy_ratios.R ${folder}
-
-
-
 
